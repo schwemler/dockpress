3e94a09cfea9f651f1afe3bf4d434d99dcc6360d
Nadobnykh <nadobnykh@gmail.com>
2019-08-01 18:25:21 +0200

    added schwemler scripts

---
+++ deploy_static_html/schwemler.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# COPY THIS SCRIPT & SET PROJECT DATA BEFORE EXEC
+# PROJECT_NAME should be the same as github user name to deploy
+
+PROJECT_NAME=schwemler
+
+PROJECT_DIR=./../${PROJECT_NAME}_static
+PROJECT_GITHUB_URL=https://github.com/${PROJECT_NAME}/${PROJECT_NAME}.github.io.git
+rm -Rf $PROJECT_DIR
+mkdir -p $PROJECT_DIR
+git clone $PROJECT_GITHUB_URL $PROJECT_DIR
+rm -Rf $PROJECT_DIR/*
+#read -n 1 -s -r -p "Press any key to continue"
+cp -rf ./html_static/* $PROJECT_DIR
+#read -n 1 -s -r -p "Press any key to continue"
+cd $PROJECT_DIR
+git add .
+git status
+git commit -m "CLI Deployment"
+git push
